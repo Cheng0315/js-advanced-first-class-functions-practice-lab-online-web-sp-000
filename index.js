2bfc20ceb985ded6082bad32981fcaa4b9a66f43
Cheng Vue <csalvue@gmail.com>
2019-02-25 10:15:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 
-const logDriverNames = function logDriverNames(drivers) {
+function logDriverNames(drivers) {
   drivers.forEach(ele) {
     console.log(ele.name);
   }
