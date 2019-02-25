7b64d7b85ed3aba39be361bfa026b69a2d27b421
Cheng Vue <csalvue@gmail.com>
2019-02-25 10:31:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function logDriverNames(drivers) {
 }
 
 function logDriversByHometown(drivers, location) {
-  drivers.forEach(function (ele) {
-    if ele.hometown === location {
-      console.log(ele.name);
-    }
+  let locationDrivers = drivers.filter(function (ele) {
+    ele.hometown === location;
   });
+
+  logDriverNames(locationDrivers);
 }
